69b42f1081009d1af394c4cb74df872b27a9838e
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2023-10-10 09:04:07 +0700

    add ocelot route to orderCart api and load product to UI using mat card

--- eShopAnalysis.FrontEnd/esa/src/shopping/shopping.module.ts
+++ eShopAnalysis.FrontEnd/esa/src/shopping/shopping.module.ts
@@ -4,6 +4,7 @@ import { ShoppingComponent } from './shopping/shopping.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './shopping/product-list/product-list.component';
 import { SharedModule } from 'src/shared/shared.module';
+import { ProductListCardComponent } from './shopping/product-list/product-list-card/product-list-card.component';
 
 const shoppingRoutes: Routes = [
   {
@@ -16,7 +17,8 @@ const shoppingRoutes: Routes = [
 @NgModule({
   declarations: [
     ShoppingComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductListCardComponent
   ],
   imports: [
     CommonModule,
