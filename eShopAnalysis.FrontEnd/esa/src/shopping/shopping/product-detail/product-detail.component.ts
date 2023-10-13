0adea4416b6731f2f701efe62c12d2c6e7f0a04a
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2023-10-13 09:40:39 +0700

    + added UI for product detail and add model to cart from there,confirmCart
    + modify backedn have cartConfirmRequest, upsert method
    + can create cart and order in db from frontend
    
    + TODO modify backend to have saleId, can accept and set sale data
    + TODO change frontend cart list to view model price info and can have coupon
    + TODO make sure user is authenticated before can confirm cart

---
+++ eShopAnalysis.FrontEnd/esa/src/shopping/shopping/product-detail/product-detail.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CartHttpService } from 'src/shared/services/http/cart-http.service';
+import { IProduct } from 'src/shared/models/product.interface';
+import { ProductHttpService } from 'src/shared/services/http/product-http.service';
+import { Subscription, map } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { IProductModel } from 'src/shared/models/productModel.interface';
+
+@Component({
+  selector: 'esa-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit, OnDestroy {
+
+  constructor(private cartService: CartHttpService, 
+              private productService: ProductHttpService,
+              private route: ActivatedRoute) { }
+  productSubscription!: Subscription;
+  routeParamsSubscription!: Subscription;
+  productId!: string;
+  product!: IProduct | undefined;
+  ngOnInit(): void {
+    this.routeParamsSubscription = this.route.params.subscribe(params => {
+      this.productId = params['productId'];
+    });
+    this.productSubscription = this.productService.allProduct$.pipe(
+      map(products => products.find(product => product.productId === this.productId)),      
+      )
+      .subscribe(product => this.product = product);
+    }
+    
+    ngOnDestroy(): void {
+      this.productSubscription.unsubscribe();
+      this.routeParamsSubscription.unsubscribe();
+    }
+
+    addModelToCart(event: Event, model: IProductModel) {
+      //get the quantity from the input
+      event.preventDefault();
+      let submitBtn = event.target as HTMLButtonElement;   
+      let modelContainer = submitBtn.parentElement?.closest('div') as HTMLDivElement;
+      let modelQuantity = modelContainer.querySelector('input[name="quantity"]') as HTMLInputElement;
+      
+      
+      //construct the cartItem to add to cart
+      let cartItem = {
+        productId: this.product?.productId,
+        productModelId: model.productModelId,
+        businessKey: this.product?.businessKey,
+        quantity: parseInt(modelQuantity.value),
+        isOnSale: model.isOnSaleModel,
+        saleType: model.saleType,
+        saleValue: model.saleValueModel,
+        unitPrice: model.price,
+        finalPrice: model.price * parseInt(modelQuantity.value),
+        unitAfterSalePrice:  model.isOnSaleModel === false ? undefined : model.priceOnSaleModel,
+        finalAfterSalePrice: model.isOnSaleModel === false ? undefined : 
+                             model.priceOnSaleModel === undefined ? undefined : 
+                             model.priceOnSaleModel * parseInt(modelQuantity.value),      
+
+      };
+      //call the service and add to cart
+      this.cartService.upsertItemToCart(cartItem);
+
+      //reset the quantity input
+      modelQuantity.value = '1';
+    }
+  
+
+}
--- eShopAnalysis.FrontEnd/esa/src/shopping/shopping/product-list/product-list.component.ts
+++ eShopAnalysis.FrontEnd/esa/src/shopping/shopping/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductHttpService } from 'src/shared/services/http/product-http.service';
 import { Observable, Subscription } from 'rxjs';
 import { IProduct } from 'src/shared/models/product.interface';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'esa-product-list',
@@ -12,10 +13,14 @@ export class ProductListComponent implements OnInit {
 
   allProduct$! : Observable<IProduct[]>;
   private _subcription!: Subscription;
-  constructor(private productService: ProductHttpService) {
-    this.allProduct$ = this.productService.allProduct$;
+  constructor(private productService: ProductHttpService,private route: Router) {
+    this.allProduct$ = this.productService.allProduct$;    
   }
 
   ngOnInit(): void {
+  }
+
+  viewProductDetail(productId: string | undefined) {
+    this.route.navigate(['shopping', 'product-detail', productId]);
   }
 }
