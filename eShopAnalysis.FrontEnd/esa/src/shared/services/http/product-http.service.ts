5241207c4aaa40ce728dc1aa201a962ea90a8563
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2023-11-25 11:57:25 +0700

    + add frontent UI, paging, sorting, filter, and styling
    + add model for lazy load on frontend
    + add logic to change the request when changing select, checkbox on UI

--- eShopAnalysis.FrontEnd/esa/src/shared/services/http/product-http.service.ts
+++ eShopAnalysis.FrontEnd/esa/src/shared/services/http/product-http.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { IProduct } from 'src/shared/models/product.interface';
+import { IProduct, IProductLazyLoadRequest, OrderType, ProductPerPage, SortBy } from 'src/shared/models/product.interface';
 import { environment as env } from 'src/environments/environment';
-
 @Injectable({
   providedIn: 'root'
 })
 export class ProductHttpService implements OnInit {
 
+  lazyLoadRequestSubject!: BehaviorSubject<IProductLazyLoadRequest>;
   allProductSubject: BehaviorSubject<IProduct[]> = new BehaviorSubject<IProduct[]>([]);
-  allProduct$ = this.allProductSubject.asObservable();
+  allProduct$ = this.allProductSubject.asObservable();  
 
   constructor(private http: HttpClient) {
     // this.getProducts().subscribe((products) => {
@@ -18,6 +18,14 @@ export class ProductHttpService implements OnInit {
     // });
     //nhu nay co nghia la khi khoi tao service thi se goi ham getProducts() de lay du lieu
     //1 lan duy nhat
+    //init request
+    this.lazyLoadRequestSubject = new BehaviorSubject<IProductLazyLoadRequest>({
+      pageOffset: 1,
+      productPerPage: ProductPerPage.Sixteen,
+      sortBy: SortBy.Id,
+      orderType: OrderType.Ascending,
+      filterRequests: []
+    });
     this.GetProducts();
    }
   ngOnInit(): void {
@@ -25,7 +33,8 @@ export class ProductHttpService implements OnInit {
   }
 
   private getProducts() {
-    return this.http.get<IProduct[]>(`${env.BASEURL}/api/ProductCatalog/ProductAPI/GetAllProduct`);
+    // return this.http.get<IProduct[]>(`${env.BASEURL}/api/ProductCatalog/ProductAPI/GetAllProduct`);
+    return this.http.post<IProduct[]>(`${env.BASEURL}/api/ProductCatalog/ProductAPI/GetProductsLazyLoad`, this.lazyLoadRequestSubject.value);
   }
   
   GetProducts() {
