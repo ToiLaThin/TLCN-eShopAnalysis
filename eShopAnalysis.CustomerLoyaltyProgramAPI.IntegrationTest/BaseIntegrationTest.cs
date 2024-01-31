b1557de5ef1a3a049c3c1dbfbb6db42d36b0e851
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-01-31 23:31:22 +0700

    init integration test, creaate web app factory, partial program.cs to ref in integration test project, ...

---
+++ eShopAnalysis.CustomerLoyaltyProgramAPI.IntegrationTest/BaseIntegrationTest.cs
@@ -0,0 +1,43 @@
+﻿using eShopAnalysis.CustomerLoyaltyProgramAPI.Data;
+using eShopAnalysis.CustomerLoyaltyProgramAPI.Models;
+using eShopAnalysis.CustomerLoyaltyProgramAPI.UnitTest.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopAnalysis.CustomerLoyaltyProgramAPI.IntegrationTest
+{
+    public class BaseIntegrationTest : IClassFixture<CustomerLoyaltyProgramWebAppFactory>, IDisposable
+    {
+        private readonly IServiceScope _scope;
+        protected PostgresDbContext DbContext { get; private set; }
+
+        protected IEnumerable<RewardTransaction> DummyRewardTranData { get; set; }
+
+        protected IEnumerable<UserRewardPoint> DummyUserRewardPointData { get; set; }
+
+        protected CustomerLoyaltyProgramWebAppFactory CustomerLoyaltyProgramWebAppFactory { get; private set; }
+
+        protected BaseIntegrationTest(CustomerLoyaltyProgramWebAppFactory webAppFactory) {
+            var scope = webAppFactory.Services.CreateScope();
+            DbContext = scope.ServiceProvider.GetRequiredService<PostgresDbContext>();
+            DummyRewardTranData = DummyDataProvider.GetRewardTransactionDummyData();
+            DummyUserRewardPointData = DummyDataProvider.GetUserRewardPointDummyData();
+            CustomerLoyaltyProgramWebAppFactory = webAppFactory;
+        }
+
+        public virtual void SeedDb()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            _scope?.Dispose();
+            DbContext.Dispose();
+        }
+    }
+}
---
+++ eShopAnalysis.CustomerLoyaltyProgramAPI.IntegrationTest/CustomerLoyaltyProgramWebAppFactory.cs
@@ -0,0 +1,56 @@
+﻿using eShopAnalysis.CustomerLoyaltyProgramAPI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+
+namespace eShopAnalysis.CustomerLoyaltyProgramAPI.IntegrationTest
+{
+    public class CustomerLoyaltyProgramWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().WithImage("postgres:15-alpine")
+            .Build();
+
+        public string ConnectionString
+        {
+            get {
+                return _postgreSqlContainer.GetConnectionString();
+            }
+        }
+
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+            builder.ConfigureServices(services =>
+            {
+                var descriptorType = typeof(DbContextOptions<PostgresDbContext>);
+                var descriptor = services.SingleOrDefault(s => s.ServiceType == descriptorType);
+
+                if (descriptor != null) {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<PostgresDbContext>(options => options.UseNpgsql(_postgreSqlContainer.GetConnectionString()));
+
+            });
+        }
+
+        public Task DisposeAsync()
+        {
+            return _postgreSqlContainer.DisposeAsync().AsTask();
+        }
+
+        public Task InitializeAsync()
+        {
+            return _postgreSqlContainer.StartAsync();
+        }
+    }
+}
